a560c81579d9676df2c2ebf871ddf241e183de0a
TimdeJonge <t.dejonge@students.uu.nl>
2017-10-16 16:06:58 +0200

    vowelReplacer made

--- reader.py
+++
@@ -1,7 +0,0 @@
-g = open('nospacecorpus.txt', 'w')
-
-with open('corpus.txt', 'r') as f:
-    line = f.readline()
-    while line!='':
-        g.write(line.replace(' ', ''))
-        line = f.readline()
--- vowelChanger.py
+++
@@ -1,4 +0,0 @@
-
-
-with open('corpus.txt','r') as f:
-    line = f.readline()
